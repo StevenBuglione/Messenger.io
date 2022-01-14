ce4e7abcdeebf36582f9188aacfc580fad133da1
Steven Buglione <stevenbuglione@Stevens-Mini.fios-router.home>
2022-01-13 19:32:08 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'messenger',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
